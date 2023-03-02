db66bb64b195bfda996d9cf8a53b2ac35a6bc7b0
Aliaksandr <halaveika1988@gmail.com>
2023-03-02 20:14:35 +0100

    feat: launch import apiendpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://0vetkopzv0.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://965zimk9s0.execute-api.eu-west-1.amazonaws.com',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
